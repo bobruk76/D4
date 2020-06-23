86ffc3e5f2f66182a316894ded95a07a73ca2482
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2020-06-23 22:34:14 +0300

    bootstrap add

--- p_library/views.py
+++ p_library/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from p_library.models import Book
 
-# Create your views here.
+def books_list(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books_count = Book.objects.all().count()
+    books = Book.objects.all()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books_count": books_count,
+        "books": books,
+    }
+    return HttpResponse(template.render(biblio_data))
